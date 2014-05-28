b9a0cf2ae9ef772cfa29a2592d66222ac54fb015
Len Lorijn <len.lorijn@mediact.nl>
2014-05-28 21:40:02 +0200

    Adds a formatted date string to the video

--- toVid.py
+++ toVid.py
@@ -7,18 +7,21 @@ capture = cv.VideoCapture(0)
 fourcc = cv.cv.CV_FOURCC('X','V','I','D')
 
 
-formated_string = datetime.datetime.now().strftime("%Y-%m-%d-%H%M-%f")
+formated_date = datetime.datetime.now().strftime("%Y-%m-%d-%H%M-%f")
 
 
-out = cv.VideoWriter(formated_string + '.avi', fourcc, 20.0, (640,480))
+out = cv.VideoWriter(formated_date + '.avi', fourcc, 20.0, (640,480))
 
 while(capture.isOpened()):
   ret, frame = capture.read()
   if ret==True:
+
+    date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    cv.putText(frame, date, (320,480), cv.FONT_HERSHEY_PLAIN, 1, 255)
+
     out.write(frame) 
 
-    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    cv.imshow('frame', gray)
+    cv.imshow('frame', frame)
 
   if cv.waitKey(1) & 0xFF == ord('q'):
     break
