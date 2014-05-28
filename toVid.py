05c666e5e8c4669943c46299835cfeacfc1ba2d2
Len Lorijn <len.lorijn@mediact.nl>
2014-05-28 18:03:09 +0200

    added a window for live viewing

--- toVid.py
+++ toVid.py
@@ -2,7 +2,7 @@ import sys
 import cv2 as cv
 import datetime
 
-capture = cv.VideoCapture(1)
+capture = cv.VideoCapture(0)
 
 fourcc = cv.cv.CV_FOURCC('X','V','I','D')
 
@@ -15,6 +15,10 @@ out = cv.VideoWriter(formated_string + '.avi', fourcc, 20.0, (640,480))
 while(capture.isOpened()):
   ret, frame = capture.read()
   if ret==True:
-    out.write(frame)
-  else:
+    out.write(frame) 
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    cv.imshow('frame', gray)
+
+  if cv.waitKey(1) & 0xFF == ord('q'):
     break
