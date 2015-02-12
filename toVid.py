326b03ecf6063a2d9dad38756cfa145e9ea88cd4
Len Lorijn <len.lorijn@mediact.nl>
2015-02-12 22:57:37 +0100

    spacing and indentation

--- toVid.py
+++ toVid.py
@@ -1,19 +1,18 @@
 import sys
 import cv2 as cv
 import datetime
-
 capture = cv.VideoCapture(0)
 
 fourcc = cv.cv.CV_FOURCC('X','V','I','D')
 
-
 formated_date = datetime.datetime.now().strftime("%Y-%m-%d-%H%M-%f")
-
 
 out = cv.VideoWriter(formated_date + '.avi', fourcc, 20.0, (640,480))
 
 while(capture.isOpened()):
+
   ret, frame = capture.read()
+
   if ret==True:
 
     date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
